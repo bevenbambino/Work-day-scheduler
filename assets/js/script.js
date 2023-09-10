82866078809bc7fe4cc98067a53ba8edbbb0f58a
Keven Benitez <Kevenchito24@icloud.com>
2023-09-09 23:34:39 -0700

    added js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,52 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+//naming variables
+var currentDayEl = $("#currentDay");
+// these are arrays
+var inputEls = $("input");
+var iconEls = $(".saveBtn");
+
+var today = moment();
+var todayHour = moment().format("H");
+
+// on page load, sets time blocks' background colors
+inputEls.each(function(i , input){
+  $(input).addClass(setBGColor($(input).attr("id")))
+
+})
+
+// makes all icons clickable 
+iconEls.each(function(i, icon){
+  $(icon).on("click", setStorage)
+})
+
+function setBGColor(id){
+  if (id == todayHour) {
+    return "present";
+  } else if (id < todayHour) {
+    return "past";
+  } else {
+    return "future";
+  }
+}
+
+// sets up local storage
+function setStorage(event){
+  var input = event.currentTarget.previousElementSibling.firstElementChild.value; //get correct text 
+  var id = event.currentTarget.previousElementSibling.firstElementChild.id; //gets the id of needed element
+  localStorage.setItem(id + "-text", input);
+
+}
+
+// on refresh, get from local storage and show it on input field
+function renderStorage(){
+  var times = ["09", "10", "11", "12", "13" , "14", "15", "16", "17"];
+  times.forEach(function(timeItems){
+    var savedText = localStorage.getItem(timeItems + "-text");
+    $("#"+timeItems).val(savedText);
+  })
+
+}
+
+//Sets the date
+currentDayEl.text(today.format("dddd, MMMM Do"));
+
+renderStorage();
